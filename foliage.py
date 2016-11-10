3a10e5b44b1db103bc6834bf000c28fb73a7635d
nsrivast <nsrivast@gmail.com>
2016-11-10 08:47:44 -0500

    new dates

--- foliage.py
+++ foliage.py
@@ -12,7 +12,7 @@ def download_imgs():
   base_url = 'http://www.foliagenetwork.com/images/stories/reports/{}/{}{}{}_{}.gif'
   
   regions = ['northeast_us', 'midwest_us', 'southeast_us']
-  months = ['%02d' % mm for mm in range(10, 12)]
+  months = ['%02d' % mm for mm in range(11, 12)]
   days = ['%02d' % dd for dd in range(1, 32)]
 #  years = [str(yyyy) for yyyy in range(2008, 2017)]
   years = [str(yyyy) for yyyy in range(2016, 2017)]
